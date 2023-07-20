44b5d87cfcca8cf648f1505ac712ddc0e423b58c
Kaj0 <sasha.kajo@gmail.com>
2023-07-20 15:18:20 -0700

    updated

--- main.c
+++ main.c
@@ -12,14 +12,23 @@ int main(int argc, char **argv)
 	char *prompt = "kajoseph $";
 	char *linepr;
 	size_t n = 0;
+	ssize_t nd_prg;
 
 	(void)argv;
 	(void)argc;
 
-	printf("%s", prompt);
-	getline(&linepr, &n, stdin);
-	printf("%s\n", linepr);
+	while (1)
+	{
+		printf("%s", prompt);
+		nd_prg = getline(&linepr, &n, stdin);
+		if (nd_prg == -1)
+		{
+			printf("exiting...\n");
+			return (-1);
+		}
+		printf("%s\n", linepr);
 
-	free(linepr);
+		free(linepr);
+	}
 	return (0);
 }
